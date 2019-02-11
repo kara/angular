78177b3008fde8ea73a49ba2fd68b86c0e2a6661
Pete Bacon Darwin <pete@bacondarwin.com>
2019-02-11 16:16:06 +0000

    fixup! refactor(compiler): wrap the jit evaluation in an injectable class

--- packages/compiler/src/jit_compiler_facade.ts
+++ packages/compiler/src/jit_compiler_facade.ts
@@ -155,8 +155,7 @@ export class CompilerFacadeImpl implements CompilerFacade {
    *
    * @param def the definition which will be compiled and executed to get the value to patch
    * @param context an object map of @angular/core symbol names to symbols which will be available
-   * in
-   * the context of the compiled expression
+   * in the context of the compiled expression
    * @param sourceUrl a URL to use for the source map of the compiled expression
    * @param preStatements a collection of statements that should be evaluated before the expression.
    */
